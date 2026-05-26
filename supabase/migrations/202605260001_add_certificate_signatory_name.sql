a2fb1deef23541de28506ece7b99c6f7c40e2a77
hamsoool <hanslacuesta@gmail.com>
2026-05-27 01:57:22 +0800

    cleaned

--- supabase/migrations/202605260001_add_certificate_signatory_name.sql
+++
@@ -1,5 +0,0 @@
-alter table public.certificates
-  add column if not exists signatory_name text;
-
-comment on column public.certificates.signatory_name is
-  'Display name of the physician selected as the medical clearance signatory.';
